ac1e6ad79e85b32777a221925746f2087f0216c1
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-09-25 09:44:04 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace spremanjeUcsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtGodnaRodjenja = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpis = new System.Windows.Forms.Button();
+            this.btnBrisanje = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(194, 81);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(265, 26);
+            this.txtIme.TabIndex = 0;
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(194, 137);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(265, 26);
+            this.txtPrezime.TabIndex = 1;
+            // 
+            // txtGodnaRodjenja
+            // 
+            this.txtGodnaRodjenja.Location = new System.Drawing.Point(194, 193);
+            this.txtGodnaRodjenja.Name = "txtGodnaRodjenja";
+            this.txtGodnaRodjenja.Size = new System.Drawing.Size(265, 26);
+            this.txtGodnaRodjenja.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(194, 251);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(265, 26);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Prezime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Godina rođenja";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "E-mail";
+            // 
+            // btnUpis
+            // 
+            this.btnUpis.Location = new System.Drawing.Point(44, 338);
+            this.btnUpis.Name = "btnUpis";
+            this.btnUpis.Size = new System.Drawing.Size(86, 42);
+            this.btnUpis.TabIndex = 8;
+            this.btnUpis.Text = "Upis";
+            this.btnUpis.UseVisualStyleBackColor = true;
+            this.btnUpis.Click += new System.EventHandler(this.btnUpis_Click);
+            // 
+            // btnBrisanje
+            // 
+            this.btnBrisanje.Location = new System.Drawing.Point(203, 338);
+            this.btnBrisanje.Name = "btnBrisanje";
+            this.btnBrisanje.Size = new System.Drawing.Size(76, 42);
+            this.btnBrisanje.TabIndex = 9;
+            this.btnBrisanje.Text = "Brisanje";
+            this.btnBrisanje.UseVisualStyleBackColor = true;
+            this.btnBrisanje.Click += new System.EventHandler(this.btnBrisanje_Click);
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(366, 338);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(81, 42);
+            this.btnSpremi.TabIndex = 10;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // txtIspis
+            // 
+            this.txtIspis.Location = new System.Drawing.Point(640, 81);
+            this.txtIspis.Multiline = true;
+            this.txtIspis.Name = "txtIspis";
+            this.txtIspis.Size = new System.Drawing.Size(399, 400);
+            this.txtIspis.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1451, 508);
+            this.Controls.Add(this.txtIspis);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.btnBrisanje);
+            this.Controls.Add(this.btnUpis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtGodnaRodjenja);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.txtIme);
+            this.Name = "Form1";
+            this.Text = "Upisivanje podataka u CSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtGodnaRodjenja;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUpis;
+        private System.Windows.Forms.Button btnBrisanje;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.TextBox txtIspis;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace spremanjeUcsv
+{
+    public partial class Form1 : Form
+    {
+        List<Osoba> listaOsoba = new List<Osoba>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrisanje_Click(object sender, EventArgs e)
+        {
+            txtEmail.Clear();
+            txtGodnaRodjenja.Clear();
+            txtIme.Clear();
+            txtPrezime.Clear();
+        }
+
+        private void btnUpis_Click(object sender, EventArgs e)
+        {
+            // Osoba osoba = new Osoba();
+            try {
+                
+                Osoba osoba = new Osoba(txtIme.Text,
+                    txtPrezime.Text,
+                    txtEmail.Text,
+                    Convert.ToInt16(txtGodnaRodjenja.Text));
+                /*
+                osoba.Ime = txtIme.Text;
+                osoba.Prezime = txtPrezime.Text;
+                osoba.Email = txtEmail.Text;
+                osoba.GodinaRodjenja = Convert.ToInt16(txtGodnaRodjenja.Text);
+                */
+                txtEmail.Clear();
+                txtGodnaRodjenja.Clear();
+                txtIme.Clear();
+                txtPrezime.Clear();
+                txtIme.Focus();
+
+                DialogResult upit = MessageBox.Show("Želite li unesti još podataka?","Upit",
+                    MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+
+                switch(upit)
+                {
+                    case DialogResult.Yes:
+                        {
+                            listaOsoba.Add(osoba);
+                            break;
+                        }
+                        case DialogResult.No:
+                        {
+                            listaOsoba.Add(osoba);
+                            txtIme.Enabled = false;
+                            txtPrezime.Enabled = false;
+                            txtGodnaRodjenja.Enabled = false;
+                            txtEmail.Enabled = false;
+                            
+                            break;
+                        }
+                }
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message, "Pogrešan unos",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                    
+            }
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            /*
+            // txtIspis.Text = osoba.ToString();
+            txtIspis.Text = "Ime,Prezime,Email,GodinaRodjenja" + Environment.NewLine;
+
+            foreach (Osoba osoba in listaOsoba)
+            {
+                txtIspis.AppendText(osoba.ToString() + Environment.NewLine);
+            }
+            */
+            string putanjaDoDatoteke = "C: \\Users\\Ucenik\\Documents\\CSV";
+            try
+            {
+                //stvaramo ili otvaramo datoteku za pisanje
+                using (StreamWriter sw = new StreamWriter(putanjaDoDatoteke))
+                {
+                    //zaglavlje (imena stupaca)
+                    sw.WriteLine("Ime,Prezime,Email,GodinaRodjenja");
+
+                    //interiramo kroz listu osoba i zapisujemo podatke u datoteku
+                    foreach (Osoba osoba in listaOsoba)
+                    {
+                        sw.WriteLine(osoba.ToString());
+                    }
+                }
+
+                MessageBox.Show("Podaci su uspješno spremljeni u CSV datoteku!", "Uspjeh",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }  
+            catch (Exception ex)
+            {
+                MessageBox.Show("Doščo je do pogrreške prilikom spremanja podataka: " + ex.Message,
+                    "Pogreška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }   
+            
+            }
+        }
+    }
+}
